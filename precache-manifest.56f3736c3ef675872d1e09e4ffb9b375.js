264d41deb741a7e8f84a1a50f34d7d5f92ff406e
Saurav kumar <sauravkhdoolia@gmail.com>
2020-03-25 16:11:55 +0530

    Updates

---
+++ precache-manifest.56f3736c3ef675872d1e09e4ffb9b375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2f5dfd681ec1d63490bfa49b0d7238",
+    "url": "/weatherapp-react-redux-immutable/index.html"
+  },
+  {
+    "revision": "89bc8331c6bd38b1485d",
+    "url": "/weatherapp-react-redux-immutable/static/css/main.02c0bc0d.chunk.css"
+  },
+  {
+    "revision": "e3abd526e58ac3746931",
+    "url": "/weatherapp-react-redux-immutable/static/js/2.ac350f3c.chunk.js"
+  },
+  {
+    "revision": "dc298e637cd0d7c5b7ecb7e1e48fb565",
+    "url": "/weatherapp-react-redux-immutable/static/js/2.ac350f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89bc8331c6bd38b1485d",
+    "url": "/weatherapp-react-redux-immutable/static/js/main.a32159f3.chunk.js"
+  },
+  {
+    "revision": "1c49f0c01d5241ac206c",
+    "url": "/weatherapp-react-redux-immutable/static/js/runtime-main.78d67902.js"
+  }
+]);
